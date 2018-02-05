a240e015eb211918d784241d74b7eda68beaa916
Alex Machado <amachado@sva.edu>
2018-02-04 20:42:00 -0500

    populate header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+var header = document.querySelector('header');
+var section = document.querySelector('section');
 
+var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 var request = new XMLHttpRequest();
 
 request.open('GET', requestURL);
@@ -12,4 +14,14 @@ request.onload = function() {
   var superHeros = request.response;
   populateHeader(superHeros);
   showHeroes(superHeros);
+}
+
+function populateHeader(jsonObj) {
+  var myH1 = document.createElement('h1');
+  myH1.textContent = jsonObj['squadName'];
+  header.appendChild(myH1);
+
+  var myPara = document.createElement('p');
+  myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+  header.appendChild(myPara);
 }
